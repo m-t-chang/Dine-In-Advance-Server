acb8052117e65e48ac3bd8fe02724a36903fbdd0
mtchang <michaeltgchang@gmail.com>
2022-01-17 12:33:51 +0800

    connect to hosted MongoDB Atlas

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
 // Config
-const mongoURI = "mongodb://127.0.0.1:27017/dine-in-advance";
-// const mongoURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.g7nbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//const mongoURI = "mongodb://127.0.0.1:27017/dine-in-advance";
+const mongoURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.g7nbg.mongodb.net/dine-in-advance?retryWrites=true&w=majority`;
 
 // Connect to Mongo
 mongoose.connect(
