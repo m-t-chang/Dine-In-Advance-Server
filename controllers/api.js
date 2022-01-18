8a6b6ac3614446ad981e4e4c41b9da3a426bc7f5
r3x6 <rexchin94@gmail.com>
2022-01-18 11:53:47 +0800

    added seed route

--- controllers/api.js
+++ controllers/api.js
@@ -7,95 +7,95 @@ const router = express.Router();
 //////////////////////////////////////////
 
 router.get("/checkAvailable", (req, res) => {
-    console.log("route reached");
-    res.json({ payload: "hello world!" });
+  console.log("route reached");
+  res.json({ payload: "hello world!" });
 });
 
 // Expected Request is something like
 //      {{server}}/api/booking?id=12345
 // Returns the booking with the ID. But it's still structured like the Restaurant schema
 router.get("/booking", (req, res, next) => {
-    console.log("API: get booking");
+  console.log("API: get booking");
 
-    // check for missing ID
-    if (typeof req.query.id === "undefined") {
-        res.status(400).json({
-            status: 400,
-            message: "Error. No reservation ID provided.",
-        });
-        next();
-        return;
-    }
+  // check for missing ID
+  if (typeof req.query.id === "undefined") {
+    res.status(400).json({
+      status: 400,
+      message: "Error. No reservation ID provided.",
+    });
+    next();
+    return;
+  }
 
-    // find the ID, and send response
-    Restaurant.find({ "bookings._id": req.query.id })
-        .select({ "bookings.$": 1 })
-        .exec((err, restaurant) => {
-            if (err) {
-                res.send(err);
-            } else {
-                res.json(restaurant);
-            }
-        });
+  // find the ID, and send response
+  Restaurant.find({ "bookings._id": req.query.id })
+    .select({ "bookings.$": 1 })
+    .exec((err, restaurant) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(restaurant);
+      }
+    });
 });
 
 // Expected Request Body: (similar to a "booking" object in schema)
 const examplePostBody = {
-    restaurantName: "Chang & Chin",
-    newBooking: {
-        tableNumber: 1,
-        customerInfo: {
-            name: "Elon Musk",
-            email: "elon@tesla.com",
-            contactNo: "510 555 1111",
-        },
-        groupSize: 2,
-        specialRequests: "",
-        date: 1644508800,
-        hoursBooked: [16],
-        deletedFlag: false,
+  restaurantName: "Chang & Chin",
+  newBooking: {
+    tableNumber: 1,
+    customerInfo: {
+      name: "Elon Musk",
+      email: "elon@tesla.com",
+      contactNo: "510 555 1111",
     },
+    groupSize: 2,
+    specialRequests: "",
+    date: 1644508800,
+    hoursBooked: [16],
+    deletedFlag: false,
+  },
 };
 
 router.post("/booking", async (req, res) => {
-    console.log("API: post booking with body: ", req.body);
-    // Server will need to insert default values for things...
-    //  find a table number for it, based on the restaurant, date, group size, and hours booked
-    //          function (restaurant, date, groupSize, hoursBooked) {
-    //                    if found, return tableNumber
-    //                      otherwise return "no table found"
-    //                }
-    //  give a reservation ID
-    //  deletedFlag = false
+  console.log("API: post booking with body: ", req.body);
+  // Server will need to insert default values for things...
+  //  find a table number for it, based on the restaurant, date, group size, and hours booked
+  //          function (restaurant, date, groupSize, hoursBooked) {
+  //                    if found, return tableNumber
+  //                      otherwise return "no table found"
+  //                }
+  //  give a reservation ID
+  //  deletedFlag = false
 
-    // insert into database
-    try {
-        const doc = await Restaurant.findOne({
-            restaurantName: req.body.restaurantName,
-        });
-        doc.bookings.push(req.body.newBooking);
-        const savedDoc = await doc.save();
-        console.log(`Updated: ${savedDoc}`);
-        res.json(savedDoc);
-    } catch (err) {
-        console.log("ERROR: ", err.message);
-        res.status(500).send(err);
-    }
+  // insert into database
+  try {
+    const doc = await Restaurant.findOne({
+      restaurantName: req.body.restaurantName,
+    });
+    doc.bookings.push(req.body.newBooking);
+    const savedDoc = await doc.save();
+    console.log(`Updated: ${savedDoc}`);
+    res.json(savedDoc);
+  } catch (err) {
+    console.log("ERROR: ", err.message);
+    res.status(500).send(err);
+  }
 
-    //  return...
-    //      fail (status 500, with error message as JSON), or
-    //      success and the inserted object, which includes ID
+  //  return...
+  //      fail (status 500, with error message as JSON), or
+  //      success and the inserted object, which includes ID
 });
 
 router.patch("/booking", (req, res) => {
-    console.log("route reached");
-    res.json({ payload: "hello world!" });
+  console.log("route reached");
+  res.json({ payload: "hello world!" });
 });
 
 router.delete("/booking", (req, res) => {
-    // find the booking, set deletedFlag = true
-    console.log("route reached");
-    res.json({ payload: "hello world!" });
+  // find the booking, set deletedFlag = true
+  console.log("route reached");
+  res.json({ payload: "hello world!" });
 });
 
 //////////////////////////////////////////
--- server.js
+++ server.js
@@ -15,18 +15,18 @@ const mongoURI = "mongodb://127.0.0.1:27017/dine-in-advance";
 
 // Models
 const Restaurant = require("./models/restaurant.js");
-//const hotelSeed = require("./models/seed.js");
+const seed = require("./models/seed.js");
 
 // Connect to Mongo
 mongoose.connect(
-    mongoURI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
-    () => {
-        console.log("The connection with mongod is established");
-    }
+  mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("The connection with mongod is established");
+  }
 );
 
 // Error / success
@@ -55,8 +55,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", apiController);
 app.use("/db", dbController);
 
+// seed route
+app.get("/seedRestaurants", (req, res) => {
+  // seeds the data
+  Restaurant.create(seed, (err, createdRestaurants) => {
+    // logs created users
+    console.log(createdRestaurants);
+    // redirects to index
+    res.send("db seeded!");
+  });
+});
+
 // start server
 const PORT = process.env.PORT ?? 5000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
