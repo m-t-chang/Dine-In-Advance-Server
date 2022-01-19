a3a7b47e9e08dc18517a91e96496c31561a15d44
mtchang <michaeltgchang@gmail.com>
2022-01-20 00:29:19 +0800

    create a route to spam Bookings for testing

--- controllers/db.js
+++ controllers/db.js
@@ -7,6 +7,8 @@ const Booking = require("../models/booking.js");
 const seedRestaurants = require("../models/seedRestaurants.js");
 const seedBookings = require("../models/seedBookings.js");
 
+const fetch = require("node-fetch");
+
 //////////////////////////////////////////
 // ENDPOINTS
 //////////////////////////////////////////
@@ -25,6 +27,41 @@ router.get("/seed", async (req, res) => {
 
         res.json(jsonToSend);
     } catch (err) {
+        res.send(err);
+    }
+});
+
+// This will both test the POST function and also seed the DB for testing the checkAvailable functionality
+
+router.get("/test/full", async (req, res) => {
+    console.log("test: tables all full route reached");
+
+    const newBooking = {
+        customerInfo: {
+            name: "Table Hogger",
+            email: "thogger@gmail.com",
+            contactNo: "1234 5678",
+        },
+        groupSize: 1,
+        specialRequests: "haha",
+        date: "2022-01-20",
+        hoursBooked: [18],
+        restaurantName: "Chang & Chin",
+    };
+
+    try {
+        // send this to POST 12 times.
+        for (let i = 0; i < 12; i++)
+            fetch("http://localhost:5000/api/booking", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newBooking),
+            });
+        res.send("success");
+    } catch (err) {
+        console.log("error:", err);
         res.send(err);
     }
 });
